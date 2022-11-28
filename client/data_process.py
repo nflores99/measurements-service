c11fdd92db6f771b3dc83bfb8f192220cded1f29
Núria <nuria.floresespinosa@gmail.com>
2022-11-28 14:26:58 +0100

    Add timestamp, X and Y columns

--- client/data_process.py
+++ client/data_process.py
@@ -3,36 +3,41 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import json
 from pathlib import Path
+import sys
+
+X = int(sys.argv[1]) # take first imput as X
+Y = int(sys.argv[2]) # take second imput as Y
 
 p = Path(__file__).parent.resolve()
 print(p)
 f = open(str(p)+'/irtt_data.json') # Opening JSON file
 data = json.load(f) # returns JSON object as a dictionary
 f.close() # Closing file
-"""
-all = pd.DataFrame(data.items()) # create dummy dataframe
-round_trips = all[1].loc[4] # pandas series
-"""
+
 round_trips = data["round_trips"]
-data_rtt = pd.DataFrame(columns = ['rtt']) # creation of dataframe with rtt column
+data_rtt = pd.DataFrame(columns = ['rtt', 'send', 'receive', 'timestamp', 'X', 'Y']) # creation of dataframe with rtt column
 
 data_rtt_list = [round_trip['delay']['rtt'] for round_trip in round_trips] # list of rtt values
 data_rx_list = [round_trip['delay']['receive'] for round_trip in round_trips] # list of rx times
 data_tx_list = [round_trip['delay']['send'] for round_trip in round_trips] # list of tx times
+data_timestamps_list = [round_trip['timestamps']['client']['send']['wall'] for round_trip in round_trips] # list of timestamps
+#print(data_timestamps_list)
 
-data_df = pd.DataFrame({'rtt':data_rtt_list, 'receive':data_rx_list, 'send':data_tx_list})
+L = len(round_trips)
+X_list = [X] * L # create a list with X value and same elements as round_trips
+Y_list = [Y] * L # create a list with Y value and same elements as round_trips
 
-"""
-for iter, val in enumerate(round_trips): # add rtt values for each packet
-    a = round_trips[iter]
-    data_rtt = data_rtt.append({'rtt':a['delay']['rtt']}, ignore_index=True)
-"""
+data_df = pd.DataFrame({'rtt':data_rtt_list, 
+                        'send':data_tx_list, 
+                        'receive':data_rx_list, 
+                        'timestamp':data_timestamps_list, 
+                        'X':X_list, 
+                        'Y':Y_list})
 
-print(data_df)
-"""
+print(data_df.head())
+
 table_data_rtt = pa.Table.from_pandas(data_df) # create table
-pq.write_table(table_data_rtt, 'irtt_data.parquet') # create parquet file
-"""   
+pq.write_table(table_data_rtt, 'irtt_data.parquet') # create parquet file 
 
 
 
